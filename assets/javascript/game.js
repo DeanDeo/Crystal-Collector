a0d1921797f195ff5098cf3c933b8e6e4c4fc1d4
Litsis <44249106+DeanDeo@users.noreply.github.com>
2018-11-14 16:47:25 -0600

    HW submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,88 @@
-var randomResult;
-var wins = 0;
+var random_result;
 var losses = 0;
+var wins = 0;
 var previous = 0;
-// var Images = ["assets/images/red.png","assets/images/blue.png","assets/images/yellow.png", "assets/images/green.png"]
 
 
-var resetAndStart = function (){
-    $(".crystals").empty();
+$("#wins").text("Wins = " + wins);
+$("#losses").text("Losses = " + losses);
 
-    var Images = ["assets/images/red.png","assets/images/blue.png","assets/images/yellow.png", "assets/images/green.png"]
+var resetAndStart = function () {
 
-    randomResult = Math.floor(Math.random() *120 +1);
+	$(".crystals").empty();
 
-    $("#result").html('Random Result: ' + randomResult);
+	var images = [
+			'assets/images/red.png', 
+			'assets/images/blue.png', 
+			'assets/images/yellow.png', 
+			'assets/images/green.png'];
+		
+	random_result = Math.floor(Math.random() * 120 ) + 1; 
 
-    for (var i = 0; i < 4; i++){
 
-        var random = Math.floor(Math.random()*11 +1);
+	$("#result").html('Random Result: ' + random_result);
 
-        var crystal = $("<div>");
-            crystal.attr({
-                "class": 'crystal',
-                "data-random": random
+	for(var i = 0; i < 4; i++){
 
-            });
-        
-    }
+		var random = Math.floor(Math.random() * 12) + 1;
+
+		var crystal = $("<div>");
+			crystal.attr({
+				"class": 'crystal',
+				"data-random": random
+			});
+			crystal.css({
+				"background-image":"url('" + images[i] + "')",
+				"background-size":"cover"
+
+			});
+
+
+		$(".crystals").append(crystal);
+
+	}
+
+	$("#previous").html("Your Total Score is: " + previous);
+
 }
+
+
+resetAndStart();
+
+
+//functions
+
+$(document).on('click', ".crystal", function () {
+
+	var num = parseInt($(this).attr('data-random'));
+
+	previous += num;
+
+
+	$("#previous").html("Your Total score is: " + previous);
+
+
+	if(previous > random_result){
+
+		losses++;
+
+		$("#losses").html("Losses: " + losses);
+
+		previous = 0;
+
+		resetAndStart();
+
+	} 
+	else if(previous === random_result){
+
+		wins++;
+
+		$("#wins").html("Wins: " + wins);
+
+		previous = 0;
+
+		resetAndStart();
+
+	}
+
+});
